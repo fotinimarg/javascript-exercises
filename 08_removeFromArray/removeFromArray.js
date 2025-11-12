4265daaed7a568da1a99dabd1bf5aa77efff1235
fotinimarg <fotinimarg13@gmail.com>
2025-11-12 14:50:40 +0200

    Finish exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+function inArr(val, arg) {
+  return val === arg;
+}
+
+const removeFromArray = function (arr, ...theArgs) {
+  let newArr = [];
+
+  arr.forEach((item) => {
+    if (!theArgs.includes(item)) {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
 };
 
 // Do not edit below this line
