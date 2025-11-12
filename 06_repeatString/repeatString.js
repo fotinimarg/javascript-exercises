fe34d65fd31e16b199f42c808a4003b87e3c0088
fotinimarg <fotinimarg13@gmail.com>
2025-11-12 13:55:45 +0200

    Finish exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeated = "";
 
+  if (Number.isInteger(num) && num >= 0) {
+    for (let i = 0; i < num; i++) {
+      repeated = repeated + string;
+    }
+  } else {
+    return "ERROR";
+  }
+
+  return repeated;
 };
 
 // Do not edit below this line
